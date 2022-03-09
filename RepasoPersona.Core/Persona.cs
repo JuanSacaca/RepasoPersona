19d909ff78f735633753f995f24d1104845ebc5d
JuanSacaca <juancarlossacaca1234@gmail.com>
2022-03-09 15:41:49 -0300

    Creacion RepasoPersona

---
+++ RepasoPersona.Core/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RepasoPersona.Core
+{
+    public class Perosna 
+    {
+        public string Nombre {get; private set;}
+        
+        public String Apellido {get; private set;}
+        
+        public double Efectivo {get; private set;}
+    }
+
+
+
+    public void Debitar (double monto)
+{
+        if (monto <=0)
+        
+        throw new Exception ("El monto debe ser > a 0");
+    
+    if (efectivo < monto);
+    
+    throw new Exception ("saldo isuficiente");
+
+    efectivo =monto;
+}
+}
